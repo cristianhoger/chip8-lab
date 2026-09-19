e975717a5176aa61af31bf27d842f332d0d026af
Cristian Hoger <cristianhoger@gmail.com>
2026-09-19 08:42:59 +0200

    first version with 07

---
+++ src/chip8.rs
@@ -0,0 +1,70 @@
+pub struct Chip8 {
+    memory: [u8; 4096],
+    v: [u8; 16],
+    i: u16,
+    pc: u16,
+
+}
+
+impl Chip8 {
+    pub fn new() -> Self {
+        Chip8 {
+            memory: [0; 4096],
+            v: [0; 16],
+            i: 0,
+            pc: 0x200,
+        }
+    }
+
+    pub fn load_program(&mut self, program: &[u8]) {
+        let start = 0x200;
+        self.memory[start..start + program.len()].copy_from_slice(program);
+    }
+
+    pub fn fetch_opcode(&self) -> u16 {
+        let pc = self.pc as usize;
+        let high_byte = self.memory[pc]  as u16;
+        let low_byte = self.memory[pc+ 1]  as u16;
+        (high_byte << 8) | low_byte
+    }
+
+    pub fn read_opcode(&self,opcode: u16)->[u16;3]{
+        
+        let family= (opcode & 0xF000) >> 12;
+        let x= (opcode & 0x0F00) >> 8;
+        let value= opcode & 0x00FF;
+
+        [family,x,value]
+       
+    }
+
+    pub fn execute_opcode(&mut self,opcode:u16){
+        let family = (opcode & 0xF000) >> 12;
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let value = (opcode & 0x00FF) as u8;
+
+        match family {
+            0x06 => {
+                self.v[x] = value;
+            }
+
+            0x07 => {
+                self.v[x] = self.v[x]+ value;
+            }
+
+            _ => {
+                println!("Opcode no implementado: {:#06X}",opcode);
+            }
+        }
+
+    }
+
+    pub fn next_instruction(&mut self) {
+    self.pc += 2;
+    }
+
+    pub fn get_register(&self, index: usize) -> u8 {
+        self.v[index]
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod chip8;
+
+use chip8::Chip8;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut chip8= Chip8::new();
+
+    let program = [
+        0x70, 0x0A,
+        0x61, 0x05
+    ];
+
+    chip8.load_program(&program);
+
+    let opcode=chip8.fetch_opcode();
+
+     println!("Opcode leido: {:#06X}",opcode);
+
+    chip8.execute_opcode(opcode);
+
+   println!("V0= {:#04X}", chip8.get_register(0));
+
 }
